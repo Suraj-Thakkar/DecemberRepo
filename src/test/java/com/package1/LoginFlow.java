3500ba4ef08e14ebead0ce201397a934848140c3
surajthakkar <thakkarsuraj641@gmail.com>
2025-12-24 09:50:03 +0530

    added loginflow logic

--- src/test/java/com/package1/LoginFlow.java
+++ src/test/java/com/package1/LoginFlow.java
@@ -12,11 +12,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
 import java.awt.*;
-import java.rmi.registry.Registry;
 import java.time.Duration;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,52 +24,83 @@ import java.util.Date;
 import static org.monte.media.FormatKeys.*;
 import static org.monte.media.VideoFormatKeys.*;
 
-@Test
-public void MoneyAdd() throws Exception {
+public class LoginFlow {
+
+    private ScreenRecorder screenRecorder;
+
+    @Test
+    public void Login() throws Exception {
         System.out.println("--- STEP 1: INITIALIZING TEST ---");
-        startRecording("MoneyAdd_Flow");
+
+        // Now these functions are defined below!
+        startRecording("Login_Flow");
 
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080", "--remote-allow-origins=*");
+        options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 
         try {
-        System.out.println("--- STEP 2: NAVIGATING TO CASHBOOK ---");
-        driver.get("https://yogi.web.cashbook.in/login");
-        System.out.println("URL Loaded: " + driver.getCurrentUrl());
+            driver.get("https://yogi.web.cashbook.in/login");
 
-        System.out.println("--- STEP 3: ENTERING PHONE NUMBER ---");
-        WebElement phone = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("phoneNumber")));
-        phone.sendKeys("+911000112587");
-        System.out.println("Phone number entered successfully.");
+            WebElement phone = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("phoneNumber")));
+            phone.sendKeys("+911000112587");
 
-        Thread.sleep(2000);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
 
-        System.out.println("--- STEP 4: CLICKING SUBMIT ---");
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
+            WebElement otp = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='text']")));
+            otp.sendKeys("123456");
 
-        System.out.println("--- STEP 5: ENTERING OTP ---");
-        WebElement otp = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='text']")));
-        otp.sendKeys("123456");
-        System.out.println("OTP entered.");
-
-        System.out.println("--- STEP 6: VERIFYING LOGIN ---");
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Verify']"))).click();
-
-        Thread.sleep(3000);
-        System.out.println("SUCCESS: Login sequence completed.");
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Verify']"))).click();
+            Thread.sleep(3000);
 
         } catch (Exception e) {
-        System.err.println("FAILURE: Error occurred at " + driver.getCurrentUrl());
-        System.err.println("Error Message: " + e.getMessage());
-        takeScreenshot(driver, "MoneyAdd_Failure");
-        throw e;
+            takeScreenshot(driver, "Failure_Log");
+            throw e;
         } finally {
-        System.out.println("--- STEP 7: CLEANUP & CLOSING ---");
-        driver.quit();
-        stopRecording();
+            driver.quit();
+            stopRecording();
         }
-        }
+    }
+
+    // --- DEFINING THE MISSING FUNCTIONS ---
+
+    public void startRecording(String methodName) throws Exception {
+        File file = new File("./recordings/");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+
+        Rectangle captureSize = new Rectangle(0, 0, width, height);
+
+        GraphicsConfiguration gc = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice()
+                .getDefaultConfiguration();
+
+        this.screenRecorder = new ScreenRecorder(gc, captureSize,
+                new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, DepthKey, 24, FrameRateKey,
+                        Rational.valueOf(15), QualityKey, 1.0f, KeyFrameIntervalKey, 15 * 60),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey, Rational.valueOf(30)),
+                null, file);
+
+        this.screenRecorder.start();
+        System.out.println("Recording started...");
+    }
+
+    public void stopRecording() throws Exception {
+        this.screenRecorder.stop();
+        System.out.println("Recording stopped.");
+    }
+
+    public void takeScreenshot(WebDriver driver, String fileName) throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        FileUtils.copyFile(scrFile, new File("./screenshots/" + fileName + "_" + timestamp + ".png"));
+        System.out.println("Screenshot saved.");
+    }
+}
